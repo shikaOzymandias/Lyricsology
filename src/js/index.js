55c7f0c4fcc05e04e2bf09ccf17a75cf779815de
shikaOzymandias <shikaozymandias@gmail.com>
2023-12-05 19:30:06 +0330

    fetch api

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,20 @@ const urlApi = "https://api.musixmatch.com/ws/1.1/";
 const apiKey = "8a497867c325bbcf4b4c9d286f8450bb";
 let encodedString = encodeURIComponent("tool sober");
 const searchUrl = `${urlApi}track.search?q_track_artist=${encodedString}&page_size=5&page=1&apikey=${apiKey}&s_track_rating=DESC`;
-
-const searchTrack = async function () {
+const mode = {
+  mode: "cors",
+  method: "POST",
+};
+const searchMusic = async function () {
   try {
-    const res = await fetch(searchUrl);
+    const res = await fetch(searchUrl, mode);
     const data = await res.json();
 
     console.log(data);
+    console.log(res);
   } catch (error) {
     console.error("Error:", error);
   }
 };
 
-searchTrack();
+searchMusic();
