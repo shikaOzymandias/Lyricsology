186ab545c982076d1b70cd6fd8189cf4e18f5da1
shikaOzymandias <shikaozymandias@gmail.com>
2023-12-06 20:39:47 +0330

    putting a condition for errors(no tack found and Authentication failed

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,31 @@
 const urlApi = "https://api.musixmatch.com/ws/1.1/";
 const apiKey = "8a497867c325bbcf4b4c9d286f8450bb";
-let encodedString = encodeURIComponent("tool sober");
-const searchUrl = `${urlApi}track.search?q_track_artist=${encodedString}&page_size=5&page=1&apikey=${apiKey}&s_track_rating=DESC`;
-const mode = {
-  mode: "cors",
-  method: "POST",
-};
+let encodedString = encodeURIComponent("Tool sober");
+let commontrackId = "148529";
+const trackGet = "track.get?commontrack_id=";
+const trackSearch = "track.search?q_track_artist=";
+
+// for trackGet we use this:
+const searchUrl = `${urlApi}${trackGet}${commontrackId}&page_size=5&page=1&apikey=${apiKey}&s_track_rating=DESC`;
+
+// for trackSearch we use this :
+// const searchUrl = `${urlApi}${trackSearch}${encodedString}&page_size=5&page=1&apikey=${apiKey}&s_track_rating=DESC`;
+
 const searchMusic = async function () {
   try {
-    const res = await fetch(searchUrl, mode);
+    const res = await fetch(searchUrl);
     const data = await res.json();
 
-    console.log(data);
+    if (data.message.body.length === 0) {
+      throw new Error(`${data.message.header.status_code}`);
+    } else if (data.message.header.available === 0) {
+      throw new Error(`Not found Any Track!`);
+    }
+
     console.log(res);
+    console.log(data);
   } catch (error) {
-    console.error("Error:", error);
+    console.error(error);
   }
 };
 
