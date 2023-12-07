5b92cb21f80484e0d844859b809d71e918e5fc8c
shikaOzymandias <shikaozymandias@gmail.com>
2023-12-07 15:01:36 +0330

    putting fetch into a function getJSON

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 const urlApi = "https://api.musixmatch.com/ws/1.1/";
-const apiKey = "8a497867c325bbcf4b4c9d286f8450bb";
+const apiKey = "8a497867c325bbcf4b4c9d28zzzzzzzzzz6f8450bb";
 let encodedString = encodeURIComponent("Tool sober");
 let commontrackId = "148529";
 const trackGet = "track.get?commontrack_id=";
@@ -11,21 +11,30 @@ const searchUrl = `${urlApi}${trackGet}${commontrackId}&page_size=5&page=1&apike
 // for trackSearch we use this :
 // const searchUrl = `${urlApi}${trackSearch}${encodedString}&page_size=5&page=1&apikey=${apiKey}&s_track_rating=DESC`;
 
-const searchMusic = async function () {
+const getJSON = async function (url) {
   try {
-    const res = await fetch(searchUrl);
+    const res = await fetch(url);
     const data = await res.json();
 
+    // first: for when api authentication is wrong, second: for when no track found.
     if (data.message.body.length === 0) {
-      throw new Error(`${data.message.header.status_code}`);
+      throw new Error(`Status Code: ${data.message.header.status_code}`);
     } else if (data.message.header.available === 0) {
       throw new Error(`Not found Any Track!`);
     }
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
 
-    console.log(res);
+const searchMusic = async function () {
+  try {
+    const data = await getJSON(searchUrl);
+
     console.log(data);
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    console.error(`${err}`);
   }
 };
 
