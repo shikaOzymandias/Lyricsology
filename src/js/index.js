475bcbe4c7539acfcb601bf6c8868cbfcbab22bc
shikaOzymandias <shikaozymandias@gmail.com>
2023-12-07 16:21:35 +0330

    creating loadSearchResults for search result

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,16 @@
-const urlApi = "https://api.musixmatch.com/ws/1.1/";
-const apiKey = "8a497867c325bbcf4b4c9d28zzzzzzzzzz6f8450bb";
+const API_URL = "https://api.musixmatch.com/ws/1.1/";
+const apiKey = "8a497867c325bbcf4b4c9d286f8450bb";
 let encodedString = encodeURIComponent("Tool sober");
 let commontrackId = "148529";
 const trackGet = "track.get?commontrack_id=";
 const trackSearch = "track.search?q_track_artist=";
 
 // for trackGet we use this:
-const searchUrl = `${urlApi}${trackGet}${commontrackId}&page_size=5&page=1&apikey=${apiKey}&s_track_rating=DESC`;
+
+// const API_URL = `${urlApi}${trackGet}${commontrackId}&page_size=5&page=1&apikey=${apiKey}&s_track_rating=DESC`;
 
 // for trackSearch we use this :
-// const searchUrl = `${urlApi}${trackSearch}${encodedString}&page_size=5&page=1&apikey=${apiKey}&s_track_rating=DESC`;
+// const API_URL = `${urlApi}${trackSearch}${encodedString}&page_size=5&page=1&apikey=${apiKey}&s_track_rating=DESC`;
 
 const getJSON = async function (url) {
   try {
@@ -28,14 +29,29 @@ const getJSON = async function (url) {
   }
 };
 
-const searchMusic = async function () {
+const loadLyrics = async function (commontrackId) {
   try {
-    const data = await getJSON(searchUrl);
+    const data =
+      await getJSON(`${API_URL}track.lyrics.get?commontrack_id=${commontrackId}&apikey=${apiKey}
+    `);
 
     console.log(data);
   } catch (err) {
-    console.error(`${err}`);
+    console.error(`${err} 🏀🏀🏀`);
   }
 };
 
-searchMusic();
+// loadLyrics(commontrackId);
+
+const loadSearchResults = async function (query) {
+  try {
+    const data =
+      await getJSON(`${API_URL}track.search?q_track_artist=${query}&page_size=10&page=1&apikey=${apiKey}&s_track_rating=DESC
+    `);
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+loadSearchResults("pink floyd");
